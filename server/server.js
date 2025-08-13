09fed10e990d8717e69185588d6d2e5b44561021
BikaashMeher <bm244368@gmail.com>
2025-08-13 19:58:51 +0530

    'inngest'

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import { inngest, functions } from "./inngest/index.js";
+import {serve} from 'inngest/express' 
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.get("/", (req, res) => {
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
